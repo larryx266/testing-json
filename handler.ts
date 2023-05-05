9a679e9d3938da3daafbbf4d535a899abce8a9e7
Larry Xu <larryx266@gmail.com>
2023-05-05 08:03:10 -0700

    react json stuff

--- handler.ts
+++ handler.ts
@@ -334,6 +334,7 @@ app.post("/getByEmail", async function (req: Request, res: Response) {
                     //     {
                     //         error: "Could not find user with provided email"
                     //     });
+                    res.json(null);
                     return;
                 }
             }
